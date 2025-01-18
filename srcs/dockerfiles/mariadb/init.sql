ebcfa8dd38a59826c7ef6531a68e2b64fae1742f
LeondavidZipp <leondavidzipp@gmx.de>
2025-01-18 18:35:43 +0100

    cleaned init.sql

--- srcs/dockerfiles/mariadb/init.sql
+++ srcs/dockerfiles/mariadb/init.sql
@@ -2,7 +2,6 @@ CREATE USER 'db_wp_user'@'%' IDENTIFIED BY 'db_wp_pwd';
 DROP DATABASE IF EXISTS db_wp_name;
 CREATE DATABASE db_wp_name;
 GRANT ALL PRIVILEGES ON db_wp_name.* TO 'db_wp_user'@'%';
-FLUSH PRIVILEGES;
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'db_root_pw';
 FLUSH PRIVILEGES;
