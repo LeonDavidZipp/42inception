4c3aa9be563f5c143e180815b128e12ee36e6ce2
LeondavidZipp <leondavidzipp@gmx.de>
2025-01-18 15:58:54 +0100

    added root user adjustments to init.sql

--- srcs/dockerfiles/mariadb/init.sql
+++ srcs/dockerfiles/mariadb/init.sql
@@ -1,5 +1,8 @@
-CREATE USER 'db_wp_user'@'%' IDENTIFIED BY 'db_wp_pw';
+CREATE USER 'db_wp_user'@'%' IDENTIFIED BY 'db_wp_pwd';
 DROP DATABASE IF EXISTS db_wp_name;
 CREATE DATABASE db_wp_name;
 GRANT ALL PRIVILEGES ON db_wp_name.* TO 'db_wp_user'@'%';
+FLUSH PRIVILEGES;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'db_root_pw';
 FLUSH PRIVILEGES;
