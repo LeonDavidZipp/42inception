64ce2d79ca07cab12c1abc7dfd66beaee6052e83
LeondavidZipp <leondavidzipp@gmx.de>
2025-01-19 17:06:08 +0100

    db fixed again

--- srcs/dockerfiles/mariadb/init.sh
+++ srcs/dockerfiles/mariadb/init.sh
@@ -2,13 +2,16 @@
 
 PW=$(cat /run/secrets/db_wp_pwd)
 
+service mariadb start
+
 sed \
 	-e "s/db_wp_user/$DB_WP_USER/g" \
 	-e "s/db_wp_pwd/$PW/g" \
 	-e "s/db_wp_name/$DB_WP_NAME/g" \
 	/app/init.sql > /tmp/init.tmp.sql \
-	&& mysql < /tmp/init.tmp.sql
+	&& mysql < /tmp/init.tmp.sql \
+	&& rm -f /tmp/init.tmp.sql
 
-kill $(cat /run/mysqld/mysqld.pid)
+service mariadb stop
 
-mysqld
+mysqld_safe
