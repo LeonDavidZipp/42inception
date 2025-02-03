631b53686e2bf2fdaeca14308b1fa6c212d28133
LeondavidZipp <leondavidzipp@gmx.de>
2025-02-03 10:50:33 +0100

    adjusted init script && config

--- srcs/dockerfiles/mariadb/init.sql
+++ srcs/dockerfiles/mariadb/init.sql
@@ -1,6 +1,5 @@
-CREATE USER 'db_wp_user'@'%' IDENTIFIED BY 'db_wp_pwd';
-DROP DATABASE IF EXISTS db_wp_name;
-CREATE DATABASE db_wp_name;
+CREATE USER IF NOT EXISTS 'db_wp_user'@'%' IDENTIFIED BY 'db_wp_pwd';
+CREATE DATABASE db_wp_name IF NOT EXISTS;
 GRANT ALL PRIVILEGES ON db_wp_name.* TO 'db_wp_user'@'%';
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'db_root_pwd';
